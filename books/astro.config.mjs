2b2a3b75e85c3660b46eb3bdda1176501da21314
jaume <jaumetruyols@paucasesnovescifp.cat>
2023-07-07 20:38:27 +0200

    intento de githubpages

--- books/astro.config.mjs
+++ books/astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  /*output: 'server',*/
+  site: 'https://github.com/JaumeTruyols/jaumetruyols.github.io.git',
+  base: '/books',
 });
