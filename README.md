be5c546289d32bed04852fd793fb93f2a06ab58e
jaume <jaumetruyols@paucasesnovescifp.cat>
2023-08-11 21:36:58 +0200

    books+node

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # proyectos-alcudiencs
 proyectos para veranear
+
+## Proyecto 1:
+### Descripción:
+**Este proyecto consiste en una proyecto de una lista de libros con animaciones utilizando Astro.**
+"Link a la carpeta books del proyecto y debajo el link a index.astro"
+[books](./books)
+
+[index.astro](books/src/pages/index.astro)
---
+++ node+strapi/web/src/app/services/videogames.js
@@ -0,0 +1,14 @@
+import {API_URL, STRAPI_URL} from "@/app/config";
+
+export async function getGames() {
+    const res = await fetch(`${API_URL}/video-games?populate[platforms][fields][0]=name&populate[cover][fields][0]=url`)
+    if (!res.ok) {
+        throw new Error('Something went wrong')
+    }
+    const {data} = await res.json()
+    return data
+}
+export function getCoverImage ({attributes}) {
+    const { url } = attributes.cover.data.attributes
+    return `${STRAPI_URL}${url}`
+}
